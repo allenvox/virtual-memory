9606c45aca7f769245edc669faff4ba35ce4c770
Yuriy Grigoryev <allenvox@icloud.com>
2023-11-10 12:09:33 +0700

    Comment code

--- src/main.c
+++ src/main.c
@@ -7,45 +7,51 @@
 #define MMU_PDE_COUNT 1024
 #define malloc_a(size, align) kmalloc_a(size, align)
 
+// page directory entry
 struct PDE {
-  uint32_t P : 1;
-  uint32_t RW : 1;
-  uint32_t US : 1;
-  uint32_t PWT : 1;
-  uint32_t PCD : 1;
-  uint32_t A : 1;
-  uint32_t D : 1;
-  uint32_t PS : 1;
-  uint32_t G : 1;
-  uint32_t reserved : 3;
-  uint32_t address : 20;
+  uint32_t P : 1; // present (is page now in memory)
+  uint32_t RW : 1; // read-write
+  uint32_t US : 1; // user/supervisor
+  uint32_t PWT : 1; // write through
+  uint32_t PCD : 1; // cache off flag
+  uint32_t A : 1; // accessed (is page in use)
+  uint32_t D : 1; // dirty (is page edited)
+  uint32_t PS : 1; // page size
+  uint32_t G : 1; // global flag (ignored)
+  uint32_t reserved : 3; // reserved bits
+  uint32_t address : 20; // page table address
 } attribute(packed);
 
+// page table entry
 struct PTE {
-  uint32_t P : 1;
-  uint32_t RW : 1;
-  uint32_t US : 1;
-  uint32_t PWT : 1;
-  uint32_t PCD : 1;
-  uint32_t A : 1;
-  uint32_t D : 1;
-  uint32_t PAT : 1;
-  uint32_t G : 1;
-  uint32_t reserved : 3;
-  uint32_t ADDRESS : 20;
+  uint32_t P : 1; // present
+  uint32_t RW : 1; // read-write
+  uint32_t US : 1; // user/supervisor
+  uint32_t PWT : 1; // write through
+  uint32_t PCD : 1; // cache off flag
+  uint32_t A : 1; // accessed
+  uint32_t D : 1; // dirty
+  uint32_t PAT : 1; // page attributes table
+  uint32_t G : 1; // global (ignored)
+  uint32_t reserved : 3; // reserved bits
+  uint32_t ADDRESS : 20; // physical address
 } attribute(packed);
 
 struct page {
   uint32_t frames[1024];
 };
 
-//Экземпляры структур с выравниванием
+// aligned structures
+// page directory
 struct PDE page_dir[MMU_PDE_COUNT] attribute(aligned(4096));
+// page table
 struct PTE page_tab[MMU_PDE_COUNT][MMU_PTE_COUNT] attribute(aligned(4096));
+// page
 struct page page_0;
-uint32_t cr3;
+uint32_t cr3; // control register 3
 
 uint32_t translate_from_logic(uint32_t logical_addr) {
+  // get physical address from logical
   uint32_t dir_addr = (cr3 >> 12) << 12;
   uint32_t cat_id = logical_addr >> 22;
   uint32_t tab_id = (logical_addr << 10) >> 22;
@@ -58,9 +64,9 @@ uint32_t translate_from_logic(uint32_t logical_addr) {
 }
 
 void mmu_print(uint32_t page_tab_count, uint32_t pages_count) {
-  uint32_t virt_addr = 0;
   printf("present\tRW\tuser_supervisor\twritethru\tcache\taccessed\tdirty\tpgsize\tignore\tpgtableaddr\n");
-  for (uint32_t i = 0; i < page_tab_count; i++) { //каталог страниц
+  for (uint32_t i = 0; i < page_tab_count; i++) {
+    // pages directory
     printf("%d\t\t", (page_dir[i]).P);
     printf("%d\t", (page_dir[i]).RW);
     printf("%d\t\t\t\t", (page_dir[i]).US);
@@ -71,10 +77,9 @@ void mmu_print(uint32_t page_tab_count, uint32_t pages_count) {
     printf("%d\t\t", (page_dir[i]).PS);
     printf("%d\t\t", (page_dir[i]).G);
     printf("0x%x\n", (uint32_t)((page_dir[i]).address));
-    // Таблица страниц
-    printf("present\tRW\tuser_supervisor\twritethru\tcache\taccessed\tdirty\tPAT\tignore\tphysaddr\tvaddr\n");
+    // pages table
+    printf("present\tRW\tuser_supervisor\twritethru\tcache\taccessed\tdirty\tPAT\tignore\tphysaddr\tvirtaddr\n");
     for (uint32_t j = 0; j < MMU_PTE_COUNT; j++) {
-      virt_addr = (i << 22) + ((j << 22) >> 10);
       printf("%d\t\t", (page_tab[i][j]).P);
       printf("%d\t", (page_tab[i][j]).RW);
       printf("%d\t\t\t\t", (page_tab[i][j]).US);
@@ -85,36 +90,38 @@ void mmu_print(uint32_t page_tab_count, uint32_t pages_count) {
       printf("%d\t", (page_tab[i][j]).PAT);
       printf("%d\t\t", (page_tab[i][j]).G);
       printf("%p\t\t", (void *)(uintptr_t)((page_tab[i][j]).ADDRESS));
-      printf("0x%x\n", virt_addr);
+      printf("0x%x\n", (uint32_t)((i << 22) + ((j << 22) >> 10)); // virtual address
     }
   }
 }
 
 int main(int argc, char **argv) {
-  uint32_t bin_size;
-  cr3 = (uintptr_t)page_dir >> 12 << 12; //кэш не предусмотрен, PAE отключено
-  if (argc > 1) {
-    if (argv[1][0] == '-') {
-      uint8_t i = 1;
-      while (argv[1][i] <= '9' && argv[1][i] >= '0') {
-        bin_size = bin_size * 10 + (uint32_t)(argv[1][i] - '0');
-        i++;
-      }
-      if (argv[1][i] == 'K' || argv[1][i] == 'k') {
-        bin_size *= 1024;
-      } else if (argv[1][i] == 'M' || argv[1][i] == 'm') {
-        bin_size *= (1024 * 1024);
-      }
+  cr3 = (uintptr_t)page_dir >> 12 << 12; // control register 3 - no cache, PAE off
+
+  uint32_t bin_size; // size of memory in bytes
+  if (argc > 1) { // if cli arg[1] exists
+    uint8_t i = 0;
+    while (argv[1][i] <= '9' && argv[1][i] >= '0') {
+      bin_size = bin_size * 10 + (uint32_t)(argv[1][i] - '0');
+      i++;
     }
-  } else {
+    if (argv[1][i] == 'K' || argv[1][i] == 'k') {
+      bin_size *= 1024;
+    } else if (argv[1][i] == 'M' || argv[1][i] == 'm') {
+      bin_size *= (1024 * 1024);
+    }
+  } else { // default - 4 KB * 16
     bin_size = 4096 * 16;
   }
+  
   uint32_t pages_count = bin_size / 4096;
   if ((bin_size % 4096) > 0)
     pages_count++;
+  
   uint32_t page_tab_count = pages_count / 1024;
   if ((pages_count % 1024) > 0)
     page_tab_count++;
+  
   for (uint16_t i = 0; i < page_tab_count; i++) {
     page_dir[i].P = 1;
     page_dir[i].RW = 1;
@@ -123,7 +130,7 @@ int main(int argc, char **argv) {
     page_dir[i].PCD = 1;
     page_dir[i].A = 0;
     page_dir[i].D = 1;
-    page_dir[i].PS = 1; /* 4KB */
+    page_dir[i].PS = 1; // 4 KB
     page_dir[i].address = (uintptr_t)(&page_tab[i]) >> 12;
     for (uint32_t j = 0; j < MMU_PTE_COUNT; j++) {
       page_tab[i][j].D = 0;
@@ -135,7 +142,7 @@ int main(int argc, char **argv) {
       page_tab[i][j].RW = 1;
       page_tab[i][j].US = 1;
       page_tab[i][j].PWT = 1;
-      page_tab[i][j].ADDRESS = ((uintptr_t)(&page_0) + j * 4096) >> 12; /* assume 4Kb pages */
+      page_tab[i][j].ADDRESS = ((uintptr_t)(&page_0) + j * 4096) >> 12; // assume 4 KB pages
     }
   }
   printf("memsize = %d, ", bin_size);
